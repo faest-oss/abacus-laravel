db1270811f7d56f8684b91bce49ad767144d0d59
Tom Dunn <tidunn13@gmail.com>
2026-09-03 22:05:11 -0400

    support pgsql in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,13 +11,31 @@ abstract class TestCase extends Orchestra
 {
     protected function defineEnvironment($app): void
     {
-        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.default', env('DB_CONN', 'sqlite'));
         $app['config']->set('database.connections.sqlite.database', ':memory:');
+
+        $app['config']->set('database.connections.pgsql', [
+            'driver'   => 'pgsql',
+            'host'     => '127.0.0.1',
+            'port'     => '54322',
+            'database' => 'abacus',
+            'username' => 'user',
+            'password' => 'password',
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'search_path' => 'public',
+        ]);
+
+        // Clone the pgsql connection for concurrency testing
+        $app['config']->set(
+            'database.connections.pgsql2',
+            $app['config']->get('database.connections.pgsql')
+        );
     }
 
     protected function defineDatabaseMigrations(): void
     {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 
     protected function getPackageProviders($app): array
